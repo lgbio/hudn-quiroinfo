3cc39bc3d3168bb329981b5b3671c0b7169d5ab5
Luis Garreta <luisgarreta@yahoo.com>
2026-05-08 09:34:23 -0500

    May/08/26: r0.901 : Renamed to hudn_quirotablero

--- dev/app_autenticacion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import vistas
-
-urlpatterns = [
-	path ('login/', vistas.LoginVista.as_view (), name='login'),
-	path ('logout/', vistas.LogoutVista.as_view (), name='logout'),
-]
